2ccf3df07d6e8cffab8386bb65843c32ee348e94
Harsh Chauhan <harshchauhan0994@gmail.com>
2021-12-13 20:26:05 +0530

    filter to search by names

--- js/script.js
+++ js/script.js
@@ -2,17 +2,17 @@
 
 let app = angular.module('MyApp', []);
 app.controller('TypischDeutschController', ($scope, $http) => {
-    $scope.user = { name: 'jon leicher' };
+    $scope.users = undefined;
+    $scope.q = undefined;
 
     // https://docs.angularjs.org/api/ng/service/$http
     // Simple GET request example:
     $http({
         method: 'GET',
-        url: 'https://randomname.de/?format=json&images=1&phone=a&email=gmail.com'
+        url: 'https://randomname.de/?format=json&count=10&images=1&phone=a&email=gmail.com'
     }).then(function successCallback(res) {
-        console.log(res.data[0]);
-        const u = res.data[0];
-        $scope.user = u;
+        console.log(res.data);
+        $scope.users = res.data;
     }, function errorCallback(err) {
         console.log(err);
     });
